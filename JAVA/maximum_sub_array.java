f0ca31a968c21b5f1c9494edbe6e2882937efb30
Sruthi A R <78865129+SruthiGIT-cloud@users.noreply.github.com>
2021-06-17 22:11:42 +0530

    Create maximum_sub_array.java

---
+++ JAVA/maximum_sub_array.java
@@ -0,0 +1,36 @@
+package program;
+import java.util.*;
+public class kadane {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		      int sum=0;
+		      int max=arr[0];
+	          for(int i=0;i<n;i++)
+	          {
+	        	  sum=sum+arr[i];
+	        	  if(sum<0)
+	        	  {
+	        		  sum=0;
+	        	  }
+	        	  if(sum>max)
+	        	  {
+	        		  max=sum;
+	        	  }
+	          }
+	          System.out.print(max);
+	    }
+	}
+/* MAXIMUM SUB ARRAY
+ * {-2, -5, 6, -2, -3, 1, 5, -6}, 
+ * 
+ * then the maximum subarray sum is 7
+ */
+
